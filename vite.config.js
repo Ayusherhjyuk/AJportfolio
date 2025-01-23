29ec1dc8077929c58678ed3c237c0498b9abe826
Ayush Jadhao <ayushjadhao8804@gmail.com>
2025-01-23 20:22:05 +0530

    permi

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',  // Expose to all interfaces
     port: process.env.PORT || 5173, // Use Render's port or default to 5173
+    allowedHosts: ['ajportfolio-4.onrender.com'], // Add your allowed host here
   },
 })
